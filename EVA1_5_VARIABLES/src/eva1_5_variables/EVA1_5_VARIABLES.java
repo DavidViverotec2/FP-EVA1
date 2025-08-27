80eee83bc9e70c8314e6a4927bc992294f83314b
DavidViverotec2 <L25550861@chihuahua2.tecnm.mx>
2025-08-27 17:53:06 -0600

    Add files via upload

---
+++ EVA1_5_VARIABLES/src/eva1_5_variables/EVA1_5_VARIABLES.java
@@ -0,0 +1,21 @@
+package eva1_5_variables;
+
+public class EVA1_5_VARIABLES {
+
+    public static void main(String[] args) {
+        // Declaracion de variables
+        // Crear las variables para que se puedan usar
+        // Tipo de dato   Identificador
+        String nombre;
+        byte edad;
+        //Inicialización
+        // Usamos el operador = Operador de asignacion
+        // Pone "David Vivero" dentro de la variable nombre
+        nombre = "David Vivero";
+        edad = 18;
+        // Uso de variables
+        System.out.println(nombre);
+        System.out.println(edad);
+    }
+    
+}
