93520f88f92ce15b67833e71cc14241eb1e35129
DavidViverotec2 <L25550861@chihuahua2.tecnm.mx>
2025-09-01 17:52:17 -0600

    Add files via upload

--- EVA1_8_TEMPERATURA/src/eva1_8_temperatura/EVA1_8_TEMPERATURA.java
+++ EVA1_8_TEMPERATURA/src/eva1_8_temperatura/EVA1_8_TEMPERATURA.java
@@ -4,13 +4,26 @@ public class EVA1_8_TEMPERATURA {
 
     public static void main(String[] args) {
         // CREACION DE VARIABLES
-        double faren,celsius;
+        double faren,celsius,kelvin;
         
-        celsius = 86;
-        faren = celsius * 1.8 + 32;
+        // ASIGNACION 
+        faren = 100;
+        celsius = (5*(faren-32))/9;
         
-        System.out.println("86 Grados celsius en farenheid son: ");
+        kelvin = celsius + 273.5;
+        
+        faren = ((9 * celsius)/5)+32;
+        
+        System.out.print("100 Grados Fahrenheit en Celsius son: ");
+        System.out.println(celsius);
+        
+        System.out.print("37.77 grados celsius  en Kelvin son: ");
+        System.out.println(kelvin);
+        
+        System.out.print("37.77 grados celsius en fahrenheit son: ");
         System.out.println(faren);
+        
+        
     }
     
 }
